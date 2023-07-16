5d1d91355b1fac82d3fcdaa2e13af1107cd66b51
marie-ming <44185091+marie-ming@users.noreply.github.com>
2023-07-16 17:00:27 +0900

    Initial commit

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  
+  ul,li {
+    list-style: none;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  button, input {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    font: inherit;
+  }
+
+  input {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default GlobalStyle;

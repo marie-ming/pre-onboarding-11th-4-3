eb50e53b3488fa31571d76eb12908e29e89e1443
marie-ming <44185091+marie-ming@users.noreply.github.com>
2023-07-18 23:03:54 +0900

    Feat : api 호출 로딩텍스트 추가

--- src/components/search/SearchRecommend.tsx
+++ src/components/search/SearchRecommend.tsx
@@ -11,17 +11,21 @@ interface PropsType {
 
 const SearchRecommend = ({ inputValue }: PropsType) => {
   const [data, setData] = useState<GetSickList[]>();
+  const [isLoading, setIsLoading] = useState(false);
 
   const debouncedText = useDebounce(inputValue);
 
   const getSick = useCallback(async () => {
     try {
+      setIsLoading(true);
       const response = await getSickList(debouncedText);
       setData(response);
 
       console.info('calling api');
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   }, [debouncedText]);
 
@@ -32,13 +36,16 @@ const SearchRecommend = ({ inputValue }: PropsType) => {
   return (
     <>
       <SearchItem text={inputValue} />
-      <TitleStyle>
-        {data?.length ? '추천 검색어' : '추천 검색어가 없습니다.'}
-      </TitleStyle>
-
-      {data?.slice(0, 7).map((item, idx) => (
-        <SearchItem key={idx} text={item.sickNm} />
-      ))}
+      <TitleStyle>추천 검색어</TitleStyle>
+      {isLoading ? (
+        <TextStyle>Loading...</TextStyle>
+      ) : data?.length ? (
+        data
+          ?.slice(0, 7)
+          .map((item, idx) => <SearchItem key={idx} text={item.sickNm} />)
+      ) : (
+        <TextStyle>추천 검색어가 없습니다.</TextStyle>
+      )}
     </>
   );
 };
@@ -50,4 +57,8 @@ const TitleStyle = styled.span`
   font-size: 13px;
 
   padding: 0 24px;
+`;
+
+const TextStyle = styled.span`
+  padding: 8px 24px;
 `;
